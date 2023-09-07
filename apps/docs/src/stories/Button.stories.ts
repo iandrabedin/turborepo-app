55c1e77e6ce62b452fa408f0fc7da63b602c1764
Iandra Bedin <iandrabedin@gmail.com>
2023-09-07 22:51:53 +0200

    fix: fix storybook import

--- apps/docs/src/stories/Button.stories.ts
+++ apps/docs/src/stories/Button.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from 'core';
+import { Button } from 'test-core-delete';
 
 const meta: Meta<typeof Button> = {
   title: 'Core/Button',
