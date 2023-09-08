121049d40a5b319f0c2f330ee9f92cd2cabcf5cf
Iandra Bedin <iandrabedin@gmail.com>
2023-09-08 16:56:42 +0200

    fix: add initial setup

--- apps/docs/storybook-static/assets/preview-7a378671.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-7164eb77.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-3c00dc74.js"),["./DocsRenderer-3PUGWF3O-3c00dc74.js","./iframe-7164eb77.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-38063e1e.js","./index-ffc7e5ff.js","./index-f5e87e53.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-7a378671.js.map
