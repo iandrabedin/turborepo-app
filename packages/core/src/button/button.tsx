5f69ef818273400c3c6dc9e654267588c6c98254
Iandra Bedin <iandrabedin@gmail.com>
2023-09-08 11:36:35 +0200

    feat(button): add variant prop and deprecate primary

--- packages/core/src/button/button.tsx
+++ packages/core/src/button/button.tsx
@@ -1,18 +1,23 @@
 interface ButtonProps {
+  /**
+   * @deprecated The prop primary will be remove in version 1.0. Use variant instead.
+   */
   primary?: boolean;
+  variant?: 'primary' | 'secondary'
   size?: "small" | "large";
   label?: string;
 }
 
 export function Button({
   primary = false,
+  variant = 'primary',
   label = "Boop",
   size = "small",
 }: ButtonProps) {
   return (
     <button
       style={{
-        backgroundColor: primary ? "red" : "blue",
+        backgroundColor: primary || variant === 'primary' ? "red" : "blue",
         fontSize: size === "large" ? "24px" : "14px",
       }}
     >
