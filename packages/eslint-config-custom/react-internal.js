5924956af5ed19a6ebfc2380b9f811fe840ec69c
Iandra Bedin <iandrabedin@gmail.com>
2023-09-08 11:50:28 +0200

    fix: ignore changelog on eslint

--- packages/eslint-config-custom/react-internal.js
+++ packages/eslint-config-custom/react-internal.js
@@ -31,7 +31,7 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.cjs"],
+  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.cjs", "CHANGELOG.md"],
 
   rules: {
     // add specific rules configurations here
